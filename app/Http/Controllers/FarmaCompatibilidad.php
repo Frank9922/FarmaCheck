fbb0d8464011cf0f827309c2a0f85da88710d418
Frank9922 <francoleiva990@gmail.com>
2024-06-28 17:20:39 -0300

    |

--- app/Http/Controllers/FarmaCompatibilidad.php
+++ app/Http/Controllers/FarmaCompatibilidad.php
@@ -23,6 +23,26 @@ class FarmaCompatibilidad extends Controller
 
     public function store(FarmaCompatibilidadRequest $request){
 
+        $farmaco1 = $request->input('first_farmaco');
+        $farmaco2 = $request->input('second_farmaco');
+
+        $resp = FarmacoCompatibilidad::where(function ( $query ) use ( $farmaco1) {
+            $query->where('first_farmaco', $farmaco1)
+            ->orWhere('second_farmaco', $farmaco1);
+                })
+                ->where( function ( $query ) use ( $farmaco2 ) {
+                        $query->where('first_farmaco', $farmaco2)
+                        ->orWhere('second_farmaco', $farmaco2);
+                })
+                ->with('compatibilidad')
+                ->with('firstFarmaco')
+                ->with('secondFarmaco')
+                ->first();
+
+        if($resp) {
+            return response()->json(['message' => 'Ya existe esta combinacion']);
+        }
+
         $row = FarmacoCompatibilidad::create($request->all());
 
         return view('FarmaCompatibildiad.succes', compact('row'));
