7e3f8c75bbd95a96c375e46f4b24f8da62b148b9
Frank9922 <francoleiva990@gmail.com>
2024-08-13 20:21:25 -0300

    -

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('/test', function() {
+    return response()->json([
+        'message' => 'ok'
+    ]);
+});
 
 Route::middleware(['auth:sanctum'])->group(function (){
 
